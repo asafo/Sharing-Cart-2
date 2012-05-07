53d3c6cdae7d9c06410e8c4daece09f733a8a15b
malu-ver2 <t-maruyama@ver2.jp>
2012-05-07 10:16:22 +0900

    Added version.php optional fields

--- blocks/sharing_cart/version.php
+++ blocks/sharing_cart/version.php
@@ -1,3 +1,6 @@
-<?php // $Id: version.php 702 2012-04-26 05:43:27Z malu $
+<?php // $Id: version.php 717 2012-05-07 01:08:09Z malu $
 
-$plugin->version = 2012042600;
+$plugin->version   = 2012042600;
+$plugin->requires  = 2011022100.00;
+$plugin->component = 'block_sharing_cart';
+$plugin->release   = '2.2 release 1';
